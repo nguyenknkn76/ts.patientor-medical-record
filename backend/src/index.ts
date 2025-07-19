63dce8a17be9df668902d9475ec0c5b812230981
Khoi Nguyen <nguyenknkn76@gmail.com>
2025-07-19 11:28:51 +0700

    uibuild

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-// import cors from 'cors';
+import cors from 'cors';
 import diagnoseRouter from './routes/diagnose.route';
 import patientRouter from './routes/patient.route';
 
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config({quiet: true});
 app.use(express.static('dist'));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 const PORT = process.env.PORT;
 
